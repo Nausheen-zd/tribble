5c8b75345844c27e0e51d79d1ae076fa6ddcaca4
Nausheen Noor Zaidi <veiledfacadeofficial@gmail.com>
2022-09-21 21:40:04 +0530

    Change

--- first.cpp
+++ first.cpp
@@ -8,17 +8,11 @@ void dec(int n){
     cout<<n<<endl;
     dec(n-1);
 }
-void inc(int n){
-    if(n==0){
-return;
-    }
-    inc(n-1);
-    cout<<n<<endl;
-}
+
 int main(){
     int n;
     cin>>n;
     dec(n);
-    inc(n);
+    
     return 0;
 }
